8b72b0e64c0d3ce689b7c0bb8d82e604577de42e
subash-5 <s44995878@gmail.com>
2022-01-22 20:05:11 +0545

    javascript

---
+++ function2.js
@@ -0,0 +1,19 @@
+function entry(name,Class,RollNo,Phone){
+    
+    console.log("student information desk:");
+    conslole.log("Name:"+name);
+    conslole.log("Class:"+Class);
+    conslole.log("RollNo:"+RollNo);
+    conslole.log("phone:"+Phone);
+
+}
+let studentname="subash sigdel";
+let studentclass="BIT-firstSemester";
+let studentrollno="5";
+let studentPhone="9841232121";
+
+entry(studentname)
+entry(studentclass)
+entry(studentrollno)
+entry(studentPhone)
+
